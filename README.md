58d51175394493c6432320415b783727e15c86a2
Chao-Kuei Hung <ckhung@cyut.edu.tw>
2016-05-29 19:25:33 +0800

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # json2dot
-Generate a .dot file from a .json file. Then one can use graphviz to create a visualization of the json structure.
+Generate a .dot file from a .json file.
+Then one can use graphviz to create
+a visualization of the json structure.
+Useful for teachers and authors to create
+illustrations, quizzes, and other pedagogical materials.
+
+## Installation
+
+On lubuntu 16.04, please do
+`apt-get install libjson-perl libfile-slurp-perl graphviz` .
+It may also work if you install these two perl modules from cpan.
+The graphviz package is needed to process a .dot file into
+a graphic file such as .svg or .jpg or .png .
+
+Copy json2dot.pl to /usr/bin and make it executable.
+
+## Usage
+
+Try it out with the included sample file favourites.geojson:
+`json2dot.pl favourites.geojson > favourites.dot ;
+dot -Tsvg favourites.dot > favourites.svg`
+Then open favourites.svg in your browser.
+You maye need to manually fix line crossings using inkscape.
+
---
+++ json2dot.pl
@@ -0,0 +1,84 @@
+#!/usr/bin/perl -w
+
+use strict;
+use JSON;
+use File::Slurp;
+binmode(STDOUT, ':utf8');
+
+my $D = read_file($ARGV[0], binmode =>':utf8');
+
+$D = from_json($D);
+
+# this is ok: print $D->{features}[0]{properties}{name};
+# this does not work no matter what: Dumper($D);
+
+my $LatestName;
+
+print <<"eof";
+digraph structs {
+
+    node [shape=plaintext]
+    rankdir=LR;
+
+    root [label=" "; shape=circle;];
+eof
+
+struct2dot("root", $D);
+
+print "}\n";
+
+sub struct2dot {
+    my ($parent, $D) = @_;
+    my ($i, $k, $c, $nbsp);
+    if (ref $D eq 'ARRAY') {
+	my $name = next_name();
+	print $name;
+	print qq([label=<\n<table border="0" cellborder="1" cellspacing="0">\n);
+	for ($i=0; $i<=$#$D; ++$i) {
+	    if (ref $D->[$i]) {
+		$c = $nbsp ? "" : "&nbsp;" x 4;
+		$nbsp = 1;
+	    } else {
+		$c = $D->[$i];
+	    }
+	    print qq(<tr><td border="0">$i</td><td PORT="p_$i">$c</td></tr>);
+	}
+	print qq(</table>>];\n\n);
+	for ($i=0; $i<=$#$D; ++$i) {
+	    struct2dot("$name:p_$i", $D->[$i]) if (ref $D->[$i]);
+	}
+	print "$parent -> $name\n";
+    } elsif (ref $D eq 'HASH') {
+	my $name = next_name();
+	print $name;
+	print qq([label=<\n<table border="0" cellborder="1" cellspacing="0">\n);
+	$i = 0;
+	my @keys = keys %$D;
+	foreach $k (@keys) {
+	    if (ref $D->{$k}) {
+		$c = $nbsp ? "" : "&nbsp;" x 4;
+		$nbsp = 1;
+	    } else {
+		$c = $D->{$k};
+	    }
+	    print qq(<tr><td>$k</td><td PORT="p_$i">$c</td></tr>);
+	    ++$i;
+	}
+	print qq(</table>>];\n\n);
+	print "$parent -> $name\n";
+	$i = 0;
+	foreach $k (@keys) {
+	    struct2dot("$name:p_$i", $D->{$k}) if (ref $D->{$k});
+	    ++$i;
+	}
+    } else {
+	print STDERR "warning: '$D' ignored\n";
+    }
+}
+
+sub next_name {
+    $LatestName = defined $LatestName ?
+	"N" . sprintf("%04d", substr($LatestName,1)+1) : 'N0000';
+    return $LatestName;
+}
+
